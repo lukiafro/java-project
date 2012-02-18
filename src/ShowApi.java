b3db4b2ba960e052f40989b7110223bd04e3eee5
afro <afro.iscience@gmail.com>
2012-02-18 19:48:01 +0100

    main class

---
+++ src/ShowApi.java
@@ -0,0 +1,19 @@
+
+import java.net.MalformedURLException;
+
+
+public class ShowApi {
+
+    /**
+     *  
+     */
+    public static void main(String[] args) throws MalformedURLException {
+        MainApi api = new MainApi();
+        api.imagePaths();
+        api.setSrodek();
+        api.Content();
+        
+
+
+    }
+}
